7defde2809bd9f7d5fe38d94f11593ad568631b7
DevWonny <cjfdnjs1994@naver.com>
2026-01-09 10:51:00 +0900

    size 변경

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import "@/styles/main.scss";
 
 export default function Main() {
   return (
-    <div className="main-wrap flex ">
+    <div className="main-wrap flex xl:gap-[50px] max-xl:gap-[20px]">
       <Introduce />
-      <div className="main-container w-full"></div>
+      <div className="main-container"></div>
     </div>
   );
 }
