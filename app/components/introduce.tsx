07d291c4cbca2409848fcf69d7e57e97d8a8e02e
DevWonny <cjfdnjs1994@naver.com>
2026-01-09 10:29:05 +0900

    Introduce Publishing End'

--- app/components/introduce.tsx
+++ app/components/introduce.tsx
@@ -4,5 +4,45 @@
 import "@/styles/components/introduce.scss";
 
 export default function Introduce() {
-  return <div className="introduce-wrap">Intro</div>;
+  return (
+    <div className="introduce-wrap flex flex-col items-center">
+      <div className="profile-container flex flex-col items-center">
+        <div className="profile-picture"></div>
+        <p className="profile-name">윤철원</p>
+        <p className="profile-category">Frontend Developer</p>
+      </div>
+
+      <div className="bot-container flex flex-col">
+        <div className="birth-container">
+          <p className="label">BIRTH</p>
+          <p className="content">1994.02.16</p>
+        </div>
+
+        <div className="phone-container">
+          <p className="label">PHONE</p>
+          <p className="content">010-9243-9717</p>
+        </div>
+
+        <div className="email-container">
+          <p className="label">EMAIL</p>
+          {/* 클릭 시 복사 */}
+          <p className="content">cjfdnjs1994@naver.com</p>
+        </div>
+
+        <div className="etc-container flex justify-between">
+          <div className="git-container flex flex-col items-center">
+            <p className="label">GITHUB</p>
+            <div className="icon">
+              {/* 아이콘 넣기. 클릭시 해당 페이지 새창 오픈 */}
+            </div>
+          </div>
+
+          <div className="resume-container flex flex-col items-center">
+            <p className="label">RESUME</p>
+            <div className="icon">{/* 아이콘 넣기. 클릭시 다운로드 */}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
