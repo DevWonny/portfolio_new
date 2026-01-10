294861135cf42c11605b4dce9cb88c1205f9b4f6
DevWonny <cjfdnjs1994@naver.com>
2026-01-10 12:37:36 +0900

    Select List 추가

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,15 @@ export default function Main() {
   return (
     <div className="main-wrap flex xl:gap-[50px] max-xl:gap-[20px]">
       <Introduce />
-      <div className="main-container"></div>
+      <div className="main-container relative">
+        <div className="select-container absolute">
+          <ul className="select-list flex h-full items-center justify-center">
+            <li className="item">About</li>
+            <li className="item">Career</li>
+            <li className="item">Portfolio</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
