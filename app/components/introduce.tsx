9ba95f18b3dd75f7e858426ecbab8368ac3e98f3
DevWonny <cjfdnjs1994@naver.com>
2026-01-14 16:46:05 +0900

    Lucide Lib Install & 아이콘 추가

--- app/components/introduce.tsx
+++ app/components/introduce.tsx
@@ -2,9 +2,11 @@
 
 // style
 import "@/styles/components/introduce.scss";
+import { Github, FileUser } from "lucide-react";
 
 export default function Introduce() {
   return (
+    // * 각 라벨 옆에 아이콘 넣기(맥북에서)!
     <div className="introduce-wrap flex flex-col items-center">
       <div className="profile-container flex flex-col items-center">
         <div className="profile-picture"></div>
@@ -32,14 +34,18 @@ export default function Introduce() {
         <div className="etc-container flex justify-between">
           <div className="git-container flex flex-col items-center">
             <p className="label">GITHUB</p>
-            <div className="icon">
+            <div className="icon flex items-center justify-center">
+              <Github />
               {/* 아이콘 넣기. 클릭시 해당 페이지 새창 오픈 */}
             </div>
           </div>
 
           <div className="resume-container flex flex-col items-center">
             <p className="label">RESUME</p>
-            <div className="icon">{/* 아이콘 넣기. 클릭시 다운로드 */}</div>
+            <div className="icon flex items-center justify-center">
+              <FileUser />
+              {/* 아이콘 넣기. 클릭시 다운로드 */}
+            </div>
           </div>
         </div>
       </div>
