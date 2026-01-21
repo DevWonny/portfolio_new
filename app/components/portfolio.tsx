e8c32ce9f01d2175463fa5826ecaa5d846a6d5cf
DevWonny <cjfdnjs1994@naver.com>
2026-01-21 10:56:34 +0900

    Label Click Event 구현'

--- app/components/portfolio.tsx
+++ app/components/portfolio.tsx
@@ -1,7 +1,20 @@
 "use client";
+import { useState } from "react";
 // style
 import "@/styles/components/portfolio.scss";
+
 export default function Portfolio() {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const onClickLabel = (labelIndex: number) => {
+    if (labelIndex === activeIndex) {
+      setActiveIndex(null);
+      return;
+    }
+
+    setActiveIndex(labelIndex);
+  };
+
   return (
     <div className="portfolio-wrap">
       <h1 className="title">Portfolio</h1>
@@ -11,14 +24,34 @@ export default function Portfolio() {
         <div className="list-container">
           <div className="company-container">
             <h2 className="label">Company Projects</h2>
-            <p className="project-label active">FanFan Project</p>
-            <p className="project-label">오토엔 Project</p>
+            <p
+              className={`project-label ${activeIndex === 1 && "active"}`}
+              onClick={() => onClickLabel(1)}
+            >
+              FanFan Project
+            </p>
+            <p
+              className={`project-label ${activeIndex === 2 && "active"}`}
+              onClick={() => onClickLabel(2)}
+            >
+              오토엔 Project
+            </p>
           </div>
 
           <div className="personal-container">
             <h2 className="label">Personal Projects</h2>
-            <p className="project-label">Bookmarker</p>
-            <p className="project-label">StockDashboard</p>
+            <p
+              className={`project-label ${activeIndex === 3 && "active"}`}
+              onClick={() => onClickLabel(3)}
+            >
+              Bookmarker
+            </p>
+            <p
+              className={`project-label ${activeIndex === 4 && "active"}`}
+              onClick={() => onClickLabel(4)}
+            >
+              StockDashboard
+            </p>
           </div>
         </div>
 
