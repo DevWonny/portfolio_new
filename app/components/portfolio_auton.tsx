50f226f2eaaf652ad324c81c767757ae4a3977ad
DevWonny <cjfdnjs1994@naver.com>
2026-01-22 10:42:42 +0900

    Company Portfolio Style 정리

--- app/components/portfolio_auton.tsx
+++ app/components/portfolio_auton.tsx
@@ -1,15 +1,51 @@
 "use client";
+import { useState } from "react";
+import { Icon } from "@iconify-icon/react";
+// style
+import "@/styles/components/portfolioCompany.scss";
 
 export default function PortfolioAuton() {
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+
+  const onDetailClick = () => {
+    if (!isDetailOpen) {
+      setIsDetailOpen(true);
+    } else {
+      setIsDetailOpen(false);
+    }
+  };
+
   return (
-    <div className="auton-container">
+    <div className="company-container flex flex-col">
       <h2 className="title">오토앤</h2>
-      <p>소속 회사 : 프래프</p>
-      <p>주요 역할 : 프론트엔드 개발</p>
-      <p>사용 스택 : React / Javascript / Axios </p>
-      <p>현재 상태 : Closed Service</p>
-      <button>상세 보기</button>
-      <div className="detail-container"></div>
+      <div className="brief-intro flex flex-col">
+        <p>소속 회사 : 프래프</p>
+        <p>주요 역할 : 프론트엔드 개발</p>
+        <p>사용 스택 : React / Javascript / Axios </p>
+        <p>현재 상태 : Closed Service</p>
+      </div>
+
+      {isDetailOpen ? (
+        <button onClick={onDetailClick}>
+          닫기
+          <Icon
+            icon="material-symbols:arrow-drop-up-rounded"
+            width="24"
+            height="24"
+          />
+        </button>
+      ) : (
+        <button onClick={onDetailClick}>
+          상세보기
+          <Icon
+            icon="material-symbols:arrow-drop-down-rounded"
+            width="24"
+            height="24"
+          />
+        </button>
+      )}
+
+      {isDetailOpen && <div className="detail-container"></div>}
     </div>
   );
 }
--- app/components/portfolio_fanfan.tsx
+++ app/components/portfolio_fanfan.tsx
@@ -1,15 +1,51 @@
 "use client";
+import { useState } from "react";
+import { Icon } from "@iconify-icon/react";
+// style
+import "@/styles/components/portfolioCompany.scss";
 
 export default function PortfolioFanfan() {
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+
+  const onDetailClick = () => {
+    if (!isDetailOpen) {
+      setIsDetailOpen(true);
+    } else {
+      setIsDetailOpen(false);
+    }
+  };
+
   return (
-    <div className="fanfan-container">
-      <h2 className="title">FanFan</h2>
-      <p>소속 회사 : 비투지 게임즈</p>
-      <p>주요 역할 : 프론트엔드 개발 및 리딩</p>
-      <p>사용 스택 : Vue.js / Nuxt.js / Typescript / Pinia / Axios </p>
-      <p>현재 상태 : Internal Project / Closed Service</p>
-      <button>상세 보기</button>
-      <div className="detail-container"></div>
+    <div className="company-container flex flex-col">
+      <h2 className="title">FanFan Project</h2>
+      <div className="brief-intro flex flex-col">
+        <p>소속 회사 : 비투지 게임즈</p>
+        <p>주요 역할 : 프론트엔드 개발 및 리딩</p>
+        <p>사용 스택 : Vue.js / Nuxt.js / Typescript / Pinia / Axios </p>
+        <p>현재 상태 : Internal Project / Closed Service</p>
+      </div>
+
+      {isDetailOpen ? (
+        <button onClick={onDetailClick}>
+          닫기
+          <Icon
+            icon="material-symbols:arrow-drop-up-rounded"
+            width="24"
+            height="24"
+          />
+        </button>
+      ) : (
+        <button onClick={onDetailClick}>
+          상세 보기
+          <Icon
+            icon="material-symbols:arrow-drop-down-rounded"
+            width="24"
+            height="24"
+          />
+        </button>
+      )}
+
+      {isDetailOpen && <div className="detail-container"></div>}
     </div>
   );
 }
